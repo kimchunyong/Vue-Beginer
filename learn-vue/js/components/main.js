c7b995e1c6786b26f72beb04401c8b59c311d5af
kimchunyong <lovemy5854@naver.com>
2018-07-25 23:00:00 +0900

    multi components

--- learn-vue/js/components/main.js
+++ learn-vue/js/components/main.js
@@ -1,3 +1,21 @@
+Vue.component('task-list', {
+  template: `
+    <div>
+      <task v-for="task in tasks">{{ task.task }}</task>
+    </div>`,
+
+  data () {
+    return {
+      tasks: [
+        { task: 'Go to the store', complate: true },
+        { task: 'Go to the email', complate: false },
+        { task: 'Go to the farm', complate: true },
+        { task: 'Go to work', complate: false }
+      ]
+    };
+  }
+});
+
 Vue.component('task', {
   template: '<li><slot></slot></li>'
 });
